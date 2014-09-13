23ed339d0d687f80ed14dfef5407bc0b3634467c
Ahmed Nadar <ahmedfay@hotmail.com>
2014-09-13 11:45:10 -0400

    Add Animal class and refactor other classes

--- zoo.rb
+++ zoo.rb
@@ -1,18 +1,16 @@
-class Panda
+class Animal
 	def likes?(food)
 		wield_food.include?(food.to_sym)
 	end
+end
 
+class Panda < Animal
 	def wield_food
 		[:bamboo]
 	end
 end
 
-class Lion
-	def likes?(food)
-		wield_food.include?(food.to_sym)
-	end
-
+class Lion < Animal
 	def wield_food
 		[:wildebeests, :zeebras]
 	end
