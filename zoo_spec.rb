6258fd3ccbf5c17983a12984539c0efcb902bf38
Ahmed Nadar <ahmedfay@hotmail.com>
2014-09-12 23:44:18 -0400

    Panda can't eat anything but Bamboo

--- zoo_spec.rb
+++ zoo_spec.rb
@@ -6,7 +6,11 @@ describe Panda do
   	expect(Panda.new.likes?(:bamboo)).to eq true
   end
 
-  it "should likes Bamboo as string" do
+  it "should like Bamboo as string" do
   	expect(Panda.new.likes?("bamboo")).to eq true
+  end
+
+  it "should not like Sushi as food" do
+  	expect(Panda.new.likes?("sushi")).to eq false
   end
 end
