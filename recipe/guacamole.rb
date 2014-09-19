04957cab3402ff964f90f2bad542a210f1c7fe3f
Ahmed Nadar <ahmedfay@hotmail.com>
2014-09-19 11:56:18 -0400

    Make Guacamole using Struct

--- recipe/guacamole.rb
+++ recipe/guacamole.rb
@@ -1,17 +1,13 @@
-ingredients = {}
-ingredients[:avocados] = 4
-ingredients[:jalapenos] = 2
-
-recipe = {}
-recipe[:ingredients] = ingredients
-recipe[:methods] = ["Peel / Slic Avocados", "Chop jalapenos into small dice"]
+# Guacamole Struct
+Recipe = Struct.new(:ingredients, :methods)
+recipe = Recipe.new({avocados: 2, jalapenos: 4}, ["Peel / Slic Avocados", "Chop jalapenos into small dice"])
 
 puts "Ingredients"
-recipe[:ingredients].each do |key, value|
+recipe.ingredients.each do |key, value|
 	puts "* #{key.capitalize}: #{value}"
 end
 
 puts "\nMethods"
-recipe[:methods].each_with_index do |step, index|
+recipe.methods.each_with_index do |step, index|
 	puts "#{index+1}. #{step}"
 end
