5cc2a0eaa245d60a6f5c73698761088bd5f6333f
Ahmed <ahmedfay@hotmail.com>
2014-12-27 23:52:15 -0500

    Change code by adding rotation to all methods

--- 05_encryptor/encryptor.rb
+++ 05_encryptor/encryptor.rb
@@ -1,27 +1,37 @@
 class Encryptor
-  def cipher
-    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
-      'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
-      'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
-      'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
-      'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
-      'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
-      'y' => 'l', 'z' => 'm'}
+  def cipher(rotation)
+    characters = (' '..'z').to_a
+    rotated_characters = characters.rotate(rotation)
+    Hash[characters.zip(rotated_characters)]
   end
 
-  def encrypt_letter(letter)
-    lowercase_letter = letter.downcase
-    cipher[lowercase_letter]
+  def reverse_rotation(rotation)
+    rotation * -1
   end
 
-  def encrypt(string)
-    # 1. Cut the input string ino letters
+  def encrypt_letter(letter, rotation)
+    cipher_for_rotation = cipher(rotation)
+    cipher_for_rotation[letter]
+  end
+
+  def encrypt(string, rotation)
     letters = string.split("")
-    # 2. Encrypt those letters one at a time, gathring the results.
-    results = []
-    letters.each do |letter|
-      encrypted_letter = encrypt_letter(letter)
-      results.push(encrypted_letter)
+
+    results = letters.collect do |letter|
+      encrypt_letter(letter, rotation)
     end
+
+    results.join
+  end
+
+
+  def decrypt(string, rotation)
+    encrypted_letters = string.split("")
+
+    results = encrypted_letters.collect do |letter|
+      encrypt_letter(letter, reverse_rotation(rotation))
+    end
+
+    results.join
   end
 end
