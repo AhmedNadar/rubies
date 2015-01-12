295c5f28b86ef25c55fbc254850a7d84cb59420a
Ahmed <ahmedfay@hotmail.com>
2015-01-11 23:55:13 -0500

    Displaying First City and State for each First name.

--- 06_event_manager/lib/event_manager.rb
+++ 06_event_manager/lib/event_manager.rb
@@ -3,6 +3,8 @@ puts "EventManager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-  name = row[2]
-  puts name
+  fname = row[:first_name]
+  city = row[:city]
+  state = row[:state]
+  puts "#{fname} from #{city} which is part of #{state} state. "
 end
